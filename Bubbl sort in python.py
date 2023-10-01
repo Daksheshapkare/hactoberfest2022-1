ff07095200f243e575f7f3cd5b7f0b8c02353c4b
Daksheshapkare <113005270+Daksheshapkare@users.noreply.github.com>
2023-10-01 17:25:05 +0530

    Create Bubbl sort in python.py

---
+++ Bubbl sort in python.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    # Traverse through all elements in the list
+    for i in range(n):
+        # Last i elements are already in place, no need to check them
+        for j in range(0, n-i-1):
+            # Swap if the element found is greater than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
